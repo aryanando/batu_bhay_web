42b8c7b248354f483aa5e7e9a89ccbc4609104de
Aryanando PC Kantor <aryanando232@gmail.com>
2024-07-30 12:04:51 +0700

    Finxing next prev

--- resources/views/vendor/binshopsblog/partials/full_post_details.blade.php
+++ resources/views/vendor/binshopsblog/partials/full_post_details.blade.php
@@ -43,7 +43,7 @@
                 </div>
             </article>
 
-            <div class="w-full flex pt-6">
+            {{-- <div class="w-full flex pt-6">
                 <a href="#" class="w-1/2 bg-white shadow hover:shadow-md text-left p-6">
                     <p class="text-lg text-blue-800 font-bold flex items-center"><i class="fas fa-arrow-left pr-1"></i>
                         Previous</p>
@@ -54,7 +54,7 @@
                             class="fas fa-arrow-right pl-1"></i></p>
                     <p class="pt-2">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</p>
                 </a>
-            </div>
+            </div> --}}
 
         </div>
     </div>
